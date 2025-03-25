4baa5568aa172cb5db35f90f05d4d3883f0c3ade
dipeshmii <155258970+dipeshmii@users.noreply.github.com>
2025-03-25 21:19:01 +0530

    Update penguin_species.py

--- penguin_species.py
+++ penguin_species.py
@@ -3,41 +3,53 @@ import pickle
 
 
 
-def get_culmen_length():
-    culmen_length = st.text_input("Culmen Length of Penguin")
-    return culmen_length
+def get_storage_temp():
+    storage_temp = st.text_input("Storage Temperature")
+    return storage_temp
 
-def get_culmen_depth():
-    culmen_depth = st.text_input("Culmen depth of Penguin")
-    return culmen_depth
+def get_storage_humid():
+    storage_humid = st.text_input("Storage Humidity")
+    return storage_humid
 
-def get_flipper_length():
-    flipper_length = st.text_input("Flipper Length")
-    return flipper_length
+def get_material_type_Glass():
+    material_type_Glass = st.text_input("material_type_Glass")
+    return material_type_Glass
 
-def get_body_mass():
-    body_mass = st.text_input("Body Mass")
-    return body_mass
+def get_material_type_HDPE():
+    material_type_HDPE = st.text_input("material_type_HDPE")
+    return material_type_HDPE
 
-def get_island_dream():
-    island_dream = st.text_input("Dream Island")
-    return island_dream
+def get_material_type_PET():
+    material_type_PET = st.text_input("material_type_PET")
+    return material_type_PET
 
-def get_island_torgersen():
-    island_torgersen = st.text_input("Torgersen Island")
-    return island_torgersen
+def get_material_type_PLA():
+    material_type_PLA	 = st.text_input("material_type_PLA")
+    return material_type_PLA
 
-def get_island_female():
-    island_female = st.text_input("Female")
-    return island_female
+def get_material_type_Paperboard():
+    material_type_Paperboard = st.text_input("material_type_Paperboard")
+    return material_type_Paperboard
 
-def get_island_male():
-    island_male = st.text_input("Male")
-    return island_male 
+def get_product_state_Semisolid():
+    product_state_Semisolid = st.text_input("product_state_Semisolid")
+    return product_state_Semisolid
 
-def predict_species(cl,cd,fl,bm,id,it,fem,mal):
-    loaded_model = pickle.load(open('decision_tree_model.pkl','rb'))
-    new_data = [[float(cl),float(cd),float(fl),float(bm),float(id),float(it),float(fem),float(mal)]]
+def get_product_state_Solid():
+    product_state_Solid = st.text_input("product_state_Solid")
+    return product_state_Solid
+
+def get_storage_cond_Chilled():
+    storage_cond_Chilled = st.text_input("storage_cond_Chilled")
+    return storage_cond_Chilled
+
+def get_storage_cond_Refrigerated():
+    storage_cond_Refrigerated = st.text_input("storage_cond_Refrigerated")
+    return storage_cond_Refrigerated
+
+def predict_species(te,hu,gl,hd,pe,pl,pp,ss,so,ch,re):
+    loaded_model = pickle.load(open('proj.pkl','rb'))
+    new_data = [[float(te),float(hu),float(gl),float(hd),float(pe),float(pl),float(pp),float(ss),float(so),float(ch),float(re)]]
     prediction = loaded_model.predict(new_data)
     st.write("Prediction with new data: ")
     st.write(prediction)
@@ -48,25 +60,20 @@ def predict_species(cl,cd,fl,bm,id,it,fem,mal):
 if __name__ == "__main__":
     st.title('Penguin Species prediction with Decision Tree model 2025')
     st.image('penguin.png')    
-    culmen_length_value = get_culmen_length()
-    culmen_depth_value = get_culmen_depth()
-    flipper_length = get_flipper_length()
-    body_mass = get_body_mass()
-    island_dream = get_island_dream()
-    island_torgersen = get_island_torgersen()
-    island_female = get_island_female()
-    island_male = get_island_male()
-    st.write("The parameters you entered are: ")
-    st.write("Culmen length ", culmen_length_value)
-    st.write("Culmen depth ", culmen_depth_value)
-    st.write("Flipper length ", flipper_length)
-    st.write("Body mass ", body_mass)
-    st.write("Dream Island ", island_dream)
-    st.write("Torgensen Island ", island_torgersen)
-    st.write("Female ", island_female)
-    st.write("Male ", island_male)
+    storage_temp = get_storage_temp()
+    storage_humid = get_storage_humid()
+    material_type_Glass = get_material_type_Glass()
+    material_type_HDPE = get_material_type_HDPE()
+    material_type_PET = get_material_type_PET()
+    material_type_PLA = get_material_type_PLA()
+    material_type_Paperboard = get_material_type_Paperboard()
+    product_state_Semisolid = get_product_state_Semisolid()
+    product_state_Solid = get_product_state_Solid()
+    storage_cond_Chilled = get_storage_cond_Chilled()
+    storage_cond_Refrigerated = get_storage_cond_Refrigerated()
+
 
 
 if st.button("Predict"):
-    predict_species(culmen_length_value,culmen_depth_value,flipper_length,body_mass,island_dream,island_torgersen,island_female,island_male)
+    predict_species(storage_temp,storage_humid,material_type_Glass,material_type_HDPE,material_type_PET,material_type_PLA,material_type_Paperboard,product_state_Semisolid,product_state_Solid,storage_cond_Chilled,storage_cond_Refrigerated)
     
